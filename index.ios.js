8e383183ca87df16ecd729ccfcb8890b511f080b
Christopher N. KATOYI-KABA <christopher.katoyi@icloud.com>
2017-07-27 22:57:37 +0200

    eslint rule

--- index.ios.js
+++ index.ios.js
@@ -8,7 +8,7 @@ import Header from './src/components/header';
 import AlbumList from './src/components/albumList';
 
 const App = () => (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
         <Header title="Albums !"/>
         <AlbumList />
     </View>
